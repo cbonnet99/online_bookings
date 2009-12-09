2335e52bcd4842339f80ad22502a4c963debb14e
Cyrille Bonnet <cbonnet99@Ramayana.local>
2009-12-09 16:05:05 +1300

    Added check for start and end times when retrieving bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,17 @@ class BookingsController < ApplicationController
   before_filter :get_selected_practitioner
   
   def index
-    @bookings = Booking.find_all_by_practitioner_id(@current_selected_pro)
+    start_time = if params[:start].blank?
+      Time.now.beginning_of_week
+    else
+      Time.zone.at(params[:start])
+    end
+    end_time = if params[:end].blank?
+      Time.now.end_of_week
+    else
+      Time.zone.at(params[:end])
+    end
+    @bookings = Booking.find_all_by_practitioner_id(@current_selected_pro, :conditions => ["starts_at BETWEEN ? AND ?", start_time, end_time] )
   end
   
   def show
--- test/functional/bookings_controller_test.rb
+++ test/functional/bookings_controller_test.rb
@@ -2,9 +2,15 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class BookingsControllerTest < ActionController::TestCase
   def test_index
-    get :index, {:practitioner_id => practitioners(:sav).permalink, :format => "json" }
+    get :index, {:practitioner_id => practitioners(:sav).permalink, :format => "json", :start => 5.days.ago.to_f, :end => 2.days.from_now.to_f}
     # puts @response.body
     assert_valid_json(@response.body)
     assert_equal 1, assigns(:bookings).size
+  end
+  def test_index_next_week
+    get :index, {:practitioner_id => practitioners(:sav).permalink, :format => "json", :start => 2.days.from_now.to_f, :end => 9.days.from_now.to_f}
+    # puts @response.body
+    assert_valid_json(@response.body)
+    assert_equal 0, assigns(:bookings).size
   end
 end
