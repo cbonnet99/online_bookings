a0f218c24f75184c8e160e9b1b7694d1b0db0d9c
Cyrille Bonnet <cbonnet99@Ramayana.local>
2010-02-14 23:00:15 +1300

    Added SMTP details

--- config/environment.rb
+++ config/environment.rb
@@ -44,4 +44,14 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+
+  config.action_mailer.smtp_settings = {
+      :enable_starttls_auto => true,
+      :address        => 'smtp.gmail.com',
+      :port           => 587,
+      :domain         => 'gmail.com',
+      :authentication => :plain,
+      :user_name      => 'colibriapp@gmail.com',
+      :password       => 'mavslr55'
+  }
 end
