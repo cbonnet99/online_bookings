d44d0b1c997511118a2b56575c2937038b50f726
Cyrille Bonnet <cbonnet99@Ramayana.local>
2010-03-06 21:22:44 +1300

    Added DB backup and backup of config file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,25 +1,18 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+set :output, "/var/log/cron_colibri.log"
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
 every 1.hour do
   runner "TaskUtils.send_reminders"
+end
+
+
+every 1.day, :at => "3am"  do
+  command "cp /etc/apache2/sites-available/redmine /home/cyrille/backups/redmine"
+  command "cp /etc/apache2/sites-available/colibri_staging /home/cyrille/backups/apache-colibri_staging"
+  command "cp /etc/apache2/sites-available/colibri_production /home/cyrille/backups/apache-colibri_production"
+  command "cp /etc/nginx/sites-available/colibri_staging /home/cyrille/backups/nginx-colibri_staging"
+  command "cp /etc/nginx/sites-available/colibri_production /home/cyrille/backups/nginx-colibri_production"
+  command "pg_dump -U colibri -d colibri_production > /home/cyrille/backups/colibri-backup-`date +\\%Y-\\%m-\\%d`.sql", :output => {:error => '/var/log/cron_colibri.log'}
+  command "pg_dump -U postgres -d redmine_production > /home/cyrille/backups/redmine-backup-`date +\\%Y-\\%m-\\%d`.sql", :output => {:error => '/var/log/cron_colibri.log'}
 end
 
 every 1.day, :at => "6 pm" do
