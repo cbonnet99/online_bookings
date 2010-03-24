cc37aaac08fa07deff26d76eb594234f4567c30f
correm <mathieu.correard@yahoo.fr>
2010-03-24 23:36:45 +0100

    added trad weekday for practicioner + shortformat date trad

--- public/javascripts/locales/messages.js
+++ public/javascripts/locales/messages.js
@@ -20,7 +20,13 @@ I18n.translations = {
 		Wednesday: "Wednesday", 
 		Thursday: "Thursday",
 		Friday: "Friday",
-		Saturday:  "Saturday"},
+		Saturday:  "Saturday",
+		/*shortMonths */
+	 	Jan: "Jan", Feb: "Feb", Mar: "Mar", Apr: "Apr",May: "May",Jun: "Jun",Jul: "Jul",Aug: "Aug",Sep: "Sep",Oct: "Oct",Nov: "Nov",Dec: "Dec",
+	     /*longMonths */
+	      January: "January",  February: "February", March: "March", April: "April", May: "May", June: "June", July: "July", August: "August", September: "September",  October: "October", November:  "November", December: "December",
+	     /*shortDays */
+	       Sun: "Sun",Mon: "Mon",Tue: "Tue",Wed: "Wed",Thu: "Thu",Fri: "Fri",Sat: "Sat"},
 		
 	fr:{
 		new_appointment: "Nouveau rendez-vous",
@@ -40,6 +46,12 @@ I18n.translations = {
 		Wednesday: "Mercredi", 
 		Thursday: "Jeudi",
 		Friday: "Vendredi",
-		Saturday:  "Samedi"
+		Saturday:  "Samedi",
+		/*shortMonths */
+	 	Jan: "Jan.", Feb: "Fev.", Mar: "Mars", Apr: "Avril",May: "Mai",Jun: "Juin",Jul: "Jul.",Aug: "Août",Sep: "Sep.",Oct: "Oct.",Nov: "Nov.",Dec: "Dec.",
+	     /*longMonths */
+	      January: "Janvier",  February: "Février", March: "Mars", April: "Avril", May: "Mai", June: "Juin", July: "Juillet", August: "Août", September: "Septembre",  October: "Octobre", November:  "Novembre", December: "Décembre",
+	     /*shortDays */
+	       Sun: "Dim.",Mon: "Lun.",Tue: "Mar.",Wed: "Mer.",Thu: "Jeu.",Fri: "Ven.",Sat: "Sam."
 	}
 		}	
