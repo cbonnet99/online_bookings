894fd9f155a2b88332833e40cb34c78194cc4231
Cyrille Bonnet <cbonnet99@Ramayana.local>
2010-10-31 21:42:05 +0100

    Test for reminder's send by email

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,12 +1,13 @@
 class Reminder < ActiveRecord::Base
   belongs_to :booking
   
+  validates_presence_of :booking
   
   named_scope :unsent, :conditions =>  "sent_at IS NULL"
   named_scope :need_sending, lambda { {:conditions =>  ["sending_at <= ? and sent_at IS NULL", Time.now.utc]} }
   
   def send_by_email!
     self.booking.send_reminder_email!
-    self.update_attribute(:sent_at, Time.now)
+    self.update_attribute(:sent_at, Time.now.in_time_zone(self.booking.practitioner.timezone))
   end
 end
--- test/unit/reminder_test.rb
+++ test/unit/reminder_test.rb
@@ -6,13 +6,31 @@ class ReminderTest < ActiveSupport::TestCase
     Time.zone = pro.timezone
 
     booking_needs_sending = Factory(:booking, :starts_at => 1.day.from_now.advance(:minutes => -30))
+    booking_needs_sending.end_grace_period!
     assert_equal 1, booking_needs_sending.reminders.size
 
     booking_does_not_need_sending = Factory(:booking, :starts_at => 1.day.from_now.advance(:minutes => 30))
+    booking_does_not_need_sending.end_grace_period!
     assert_equal 1, booking_does_not_need_sending.reminders.size
 
     reminders_need_sending = Reminder.need_sending
     
     assert_equal 1, reminders_need_sending.size
   end
+  
+  def test_sent_by_email
+    pro = Factory(:practitioner)
+    booking = Factory(:booking, :practitioner => pro)
+    booking.end_grace_period!
+    assert_equal 1, booking.reminders.size
+    last_reminder = booking.reminders.last
+    assert_nil last_reminder.sent_at
+    
+    local_time_for_pro = Time.now.in_time_zone(pro.timezone)
+    last_reminder.send_by_email!
+    
+    Time.zone = pro.timezone
+    assert_in_delta local_time_for_pro, last_reminder.sent_at, 1
+  end
+  
 end
