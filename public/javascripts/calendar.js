35e41e7975e94f80e6f38d5b1a9ef90452ace31d
Cyrille Bonnet <cbonnet99@Ramayana.local>
2010-12-08 13:28:02 +0100

    Hide event cancel HTML, move addCalendarFlash function

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -13,9 +13,4 @@ $(document).ready(function(){
 		var currentDiffMillis = new Date($("select[name='end']").val()).getTime() - new Date($("select[name='start']").val()).getTime();
 		var booking_type_id = $("select[name='booking_type_id']").val();
 	});
-	addFlashCalendar = function() {
-		if ($("#flash_calendar").length == 0){
-			$("<div id='flash_calendar'></div>").appendTo($("div.wc-nav"));
-		}
-	};
 });
