fee70b1f3f785ebdfa058f1b78c570309e6aeade
Cyrille Bonnet <cbonnet99@Ramayana.local>
2011-01-26 15:23:27 +0100

    For past reminders, start with most recent

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -11,7 +11,7 @@ class RemindersController < ApplicationController
   def index_past
     @selected_tab = "reminders"
     @reminders = current_pro.reminders.paginate :conditions => ["sent_at is not null and sending_at < ?", Time.now.in_time_zone(current_pro.timezone)],
-                                   :page => params[:page], :order => "sending_at"
+                                   :page => params[:page], :order => "sending_at desc"
   end
 
 end
