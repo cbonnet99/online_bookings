badcf93589c5ab3de09753cb79b4627fe8458af5
Cyrille Bonnet <cbonnet99@Ramayana.local>
2011-01-30 08:42:39 +0100

    Fix admin phone numbers

--- lib/administration.rb
+++ lib/administration.rb
@@ -1,5 +1,5 @@
 module Administration
-  ADMIN_PHONES = ["0760566501", "0760677393"]
+  ADMIN_PHONES = ["07-60566501", "07-60677393"]
   ADMINS = [{:email => "cbonnet99@gmail.com", :first_name => "Cyrille", :last_name => "Bonnet", :phone  => "0760566501"}]
   
 end
