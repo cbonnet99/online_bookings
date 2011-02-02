6a70d913abbbac57e43010ec4e6f4dede1f110a6
Cyrille Bonnet <cbonnet99@Ramayana.local>
2011-02-02 09:58:02 +0100

    Added better help for upgrading

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,6 +14,38 @@ $(document).ready(function() {
     round_selected();
 
 	$('.rounded').corner();
+	
+	var get_explanation_id_from_link = function(explanation_link_id){
+	    var str_to_remove = "_link";
+	    var explanation_id = explanation_link_id.substring(0, explanation_link_id.length-str_to_remove.length)
+	    return explanation_id	    
+	};
+	
+	var get_explanation_id_from_close = function(explanation_close_id){
+	    var str_to_remove = "_close";
+	    var explanation_id = explanation_close_id.substring(0, explanation_close_id.length-str_to_remove.length)
+	    return explanation_id	    
+	};
+	
+	$('.explanation').hide();
+	$('.explanation_link').click(function(event){
+	    var explanation_link_id = $(this).attr("id");
+	    var explanation_id = get_explanation_id_from_link(explanation_link_id);
+	    $("#"+explanation_id).show();
+	});
+	
+	$('.explanation_link').click(function(event){
+	    var explanation_link_id = $(this).attr("id");
+	    var explanation_id = get_explanation_id_from_link(explanation_link_id);
+	    $("#"+explanation_id).slideDown("slow");
+	});
+	
+	$('.explanation_close').click(function(event){
+	    var explanation_close_id = $(this).attr("id");
+	    var explanation_id = get_explanation_id_from_close(explanation_close_id);
+	    $("#"+explanation_id).slideUp("slow");
+	});
+	
     // $(':input').keydown(function(e) {
     //  if (e.keyCode == 13) {
     //      $("form").submit();
